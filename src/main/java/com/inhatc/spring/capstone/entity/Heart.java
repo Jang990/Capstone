6786f286232cc6168a0ace5d1ba2b04329cd34ab
Jang_Desktop <sdsd090811@gmail.com>
2022-11-04 19:14:24 +0900

    Add Heart 엔티티

--- src/main/java/com/inhatc/spring/capstone/entity/Heart.java
+++ src/main/java/com/inhatc/spring/capstone/entity/Heart.java
@@ -1,10 +1,17 @@
 package com.inhatc.spring.capstone.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.inhatc.spring.capstone.entity.project.ProjectContent;
+import com.inhatc.spring.capstone.entity.user.Users;
 
 @Entity
 @Table(name = "heart")
@@ -19,4 +26,11 @@ public class Heart {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "project_content_id")
+	private ProjectContent project;
+	
+	@OneToOne(fetch =FetchType.LAZY)
+	private Users likedUser;
 }
