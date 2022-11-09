e83380c39bd19210d446243721c2825bb593a629
Jang_Desktop <sdsd090811@gmail.com>
2022-11-09 19:25:57 +0900

    Feat: RecruitedMember 엔티티에 구인 인원 수 추가 및 연관 태그 리스트 만들어놓음

--- src/main/java/com/inhatc/spring/capstone/entity/recuit/RecruitedMember.java
+++ src/main/java/com/inhatc/spring/capstone/entity/recuit/RecruitedMember.java
@@ -1,11 +1,19 @@
 package com.inhatc.spring.capstone.entity.recuit;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.inhatc.spring.capstone.entity.Tag.PositionTag;
+import com.inhatc.spring.capstone.entity.content.Content;
 
 @Entity
 @Table(name = "recruited_member")
@@ -20,4 +28,13 @@ public class RecruitedMember {
 	@Column(name="recruited_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "content_id")
+	private Content content; // 게시글
+	
+	@OneToMany(mappedBy = "id")
+	private List<PositionTag> tags; // 사용한 태그
+	
+	private int rec_count; // 구인 인원 수
 }
