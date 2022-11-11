da15e42408ee06421241b7c9c5e46288915d63bb
Jang_Desktop <sdsd090811@gmail.com>
2022-11-11 12:02:41 +0900

    Feat: 아이콘 테스트 컨트롤러 및 뷰 추가

--- src/main/java/com/inhatc/spring/capstone/test/controller/TestController.java
+++ src/main/java/com/inhatc/spring/capstone/test/controller/TestController.java
@@ -22,4 +22,9 @@ public class TestController {
 		TestDTO testObj = testService.getTest(100L);
 		model.addAttribute("testObj", testObj);
 	}
+	
+	@GetMapping("/icon")
+	public void test2(Model model) {
+		
+	}
 }
