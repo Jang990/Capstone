991d6f60a93674b0791811464761b571a40e4559
Jang_Desktop <sdsd090811@gmail.com>
2022-11-11 18:23:46 +0900

    Feat: 아이콘 리소스 사용 View, Controller 예시 추가

--- src/main/java/com/inhatc/spring/capstone/test/controller/TestController.java
+++ src/main/java/com/inhatc/spring/capstone/test/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.inhatc.spring.capstone.test.dto.ImgLocTestDTO;
 import com.inhatc.spring.capstone.test.dto.TestDTO;
 import com.inhatc.spring.capstone.test.service.TestService;
 
@@ -25,6 +26,10 @@ public class TestController {
 	
 	@GetMapping("/icon")
 	public void test2(Model model) {
+		String java = "JAVA";
+		ImgLocTestDTO img = new ImgLocTestDTO(java.toLowerCase());
 		
+		// wordmark 사진이 있을 수도 있고 없을 수도 있음. 파일체크를 해봐야 함
+		model.addAttribute("imgLocData", img);
 	}
 }
---
+++ src/main/java/com/inhatc/spring/capstone/test/dto/ImgLocTestDTO.java
@@ -0,0 +1,10 @@
+package com.inhatc.spring.capstone.test.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ImgLocTestDTO {
+	private String imgLoc;
+}
