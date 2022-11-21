1c530a58054e4f73b83bd612196d559062e316ae
Jang_Desktop <sdsd090811@gmail.com>
2022-11-21 13:25:28 +0900

    Feat: OAuth 데이터를 전달할 DTO 추가

---
+++ src/main/java/com/inhatc/spring/capstone/user/dto/OAuthAttributes.java
@@ -0,0 +1,49 @@
+package com.inhatc.spring.capstone.user.dto;
+
+import java.util.Map;
+
+import com.inhatc.spring.capstone.user.entity.Users;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+/** OAuth 처리를 위한 DTO */
+public class OAuthAttributes {
+	private String name;
+	private String email;
+	private String picture;
+	private Map<String, Object> attributes;
+	private String nameAttributeKey;
+	
+	@Builder
+	public OAuthAttributes(String name, String email, String picture, Map<String, Object> attributes,
+			String nameAttributeKey) {
+		this.name = name;
+		this.email = email;
+		this.picture = picture;
+		this.attributes = attributes;
+		this.nameAttributeKey = nameAttributeKey;
+	}
+
+	public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+        		.name((String) attributes.get("family_name") + (String) attributes.get("given_name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+    
+    public Users toEntity() {
+    	return Users.createUser(new UsersJoinDTO(this.email, this.name, "OAuthUser"));
+    }
+
+}
