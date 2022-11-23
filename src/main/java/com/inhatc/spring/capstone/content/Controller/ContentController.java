fb3aab45c57f6af4168f6c7f9342f4438e45d568
Jang_Desktop <sdsd090811@gmail.com>
2022-11-23 18:25:08 +0900

    Feat: Content 컨트롤러 로직 작성

--- src/main/java/com/inhatc/spring/capstone/content/Controller/ContentController.java
+++ src/main/java/com/inhatc/spring/capstone/content/Controller/ContentController.java
@@ -1,8 +1,31 @@
 package com.inhatc.spring.capstone.content.Controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.inhatc.spring.capstone.content.Service.ContentService;
+import com.inhatc.spring.capstone.content.dto.ContentDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test/content")
 public class ContentController {
 	
+	private final ContentService contentService;
+	
+	@PostMapping
+	public String createContent(ContentDTO contentDto) {
+		if(contentDto.isRecruit()) {
+			// 프로젝트 구인 게시글 생성
+			contentService.createRecruitContent(contentDto);
+		}
+		else {
+			// 프로젝트 게시글 생성
+			contentService.createProjectContent(contentDto);
+		}
+		return "완료";
+	}
 }
--- src/main/java/com/inhatc/spring/capstone/content/Service/ContentService.java
+++ src/main/java/com/inhatc/spring/capstone/content/Service/ContentService.java
@@ -2,7 +2,17 @@ package com.inhatc.spring.capstone.content.Service;
 
 import org.springframework.stereotype.Service;
 
+import com.inhatc.spring.capstone.content.dto.ContentDTO;
+
 @Service
 public class ContentService {
-
+	/** 프로젝트 게시글 생성 */
+	public void createProjectContent(ContentDTO contentDto) {
+		
+	}
+	
+	/** 프로젝트 관련 구인 게시글 생성 */
+	public void createRecruitContent(ContentDTO contentDto) {
+		
+	}
 }
