f9b976bf15c16b729e3164af73bb20ee07a9b053
Jang_Desktop <sdsd090811@gmail.com>
2022-11-23 18:39:56 +0900

    Feat: 컨텐츠 생성 메서드 만듦
    기존에는 구인 컨텐츠와 프로젝트 컨텐츠를 한 컨트롤러에서 관리해보려 했는데 받아야할게 다르기 때문에 수정 필요함
    
    구인 콘텐츠 생성의 경우 콘텐츠를 저장하고
    컨트롤러로 받은 RecruitedMember에 대한 내용으로 객체를 생성하고 콘텐츠를 연결해서 저장해야 함
    
    컨트롤러 서비스 부분에 변화가 필요

--- src/main/java/com/inhatc/spring/capstone/content/entity/Content.java
+++ src/main/java/com/inhatc/spring/capstone/content/entity/Content.java
@@ -14,13 +14,17 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-
+import com.inhatc.spring.capstone.content.dto.ContentDTO;
 import com.inhatc.spring.capstone.entity.base.CreatedAndUpdated;
 import com.inhatc.spring.capstone.entity.file.SavedFile;
 import com.inhatc.spring.capstone.user.entity.Users;
 import com.inhatc.spring.capstone.util.BooleanToYNConverter;
 
+import lombok.Builder;
+import lombok.Getter;
+
 @Entity
+@Getter
 @Table(name = "content")
 /** 작성글 정보 테이블 엔티티 */
 public class Content extends CreatedAndUpdated{
@@ -47,9 +51,9 @@ public class Content extends CreatedAndUpdated{
 	private Users writer; // 게시물 작성자
 	private String title; // 제목
 	private String content; // 내용
-	private String used_language; // 사용 언어 - 나중에 GitHub API를 사용하면서 타입을 바꿀 것
-	private int view_count; // 조회수 - 쿠키로 조회수 중복을 제거할 것이다.
-	private int vote_count; // 찬반 카운트
+	private String usedLanguage; // 사용 언어 - 나중에 GitHub API를 사용하면서 타입을 바꿀 것
+	private int viewCount; // 조회수 - 쿠키로 조회수 중복을 제거할 것이다.
+	private int voteCount; // 찬반 카운트
 	
 	@Convert(converter = BooleanToYNConverter.class)
 	@Column(length = 1)
@@ -57,5 +61,33 @@ public class Content extends CreatedAndUpdated{
 	
 	@OneToMany(mappedBy = "id")
 	List<SavedFile> files = new ArrayList<>();
+	
+	public static Content craeteContent(Users writer, ContentDTO contentDto) {
+		return Content.builder()
+				.writer(writer)
+				.title(contentDto.getTitle())
+				.content(contentDto.getContent())
+				.usedLanguage(contentDto.getUsedLanguage())
+				.viewCount(0)
+				.voteCount(0)
+				.isRecruit(contentDto.isRecruit())
+				// DTO에서 엔티티 제거 후 MultipartFile 형식으로 바꾸고 여기서 SavedFile 엔티티 생성 예정
+				.files(contentDto.getFiles()) 
+				.build();
+	}
+	
+	
+	@Builder
+	public Content(Users writer, String title, String content, String usedLanguage, int viewCount, int voteCount,
+			boolean isRecruit, List<SavedFile> files) {
+		this.writer = writer;
+		this.title = title;
+		this.content = content;
+		this.usedLanguage = usedLanguage;
+		this.viewCount = viewCount;
+		this.voteCount = voteCount;
+		this.isRecruit = isRecruit;
+		this.files = files;
+	}
 	
 }
