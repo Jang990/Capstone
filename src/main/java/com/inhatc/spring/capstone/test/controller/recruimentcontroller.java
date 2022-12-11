43fc34752ae8684acfe8fd94ec660c282e19f95c
sungyeon98 <tjddus5910@gmail.com>
2022-12-11 16:35:06 +0900

    테스트중

--- src/main/java/com/inhatc/spring/capstone/test/controller/recruimentcontroller.java
+++ src/main/java/com/inhatc/spring/capstone/test/controller/recruimentcontroller.java
@@ -28,15 +28,27 @@ public class recruimentcontroller {
 		// 구인시현황을 리스트로 해야하는데 여기다가 리스트를 만들까요
 
 		
-		public recruimentDto(String title,String email,String writer,int viewCount,
-				String ends,String usedLanguage,int count) {
+		public recruimentDto(String title,String email,String writer,int viewCount
+				) {
 		this.title = title;
 		this.writer =writer;
 		this.email=email;
 		this.viewCount = viewCount;
-		this.ends = ends;
-		this.usedLanguage = usedLanguage;
-		this.count = count;
+
+		}
+	}
+	
+	@Getter
+	@Setter
+	class recruimentmemberDto{
+		int count = 0;//구인 현황 남은인원
+		String ends="프론트 엔드";// 프론트 엔드 백엔드
+		String usedLanguage = "자바";//언어
+		
+		public recruimentmemberDto(String ends,String usedLanguage,int count) {
+			this.ends = ends;
+			this.usedLanguage=usedLanguage;
+			this.count = count;
 		}
 	}
 	
@@ -45,16 +57,24 @@ public class recruimentcontroller {
 		
 
 		List<recruimentDto> recruimentList = new ArrayList<>();//프로젝트 리스트
-		recruimentDto test1 = new recruimentDto("캡스톤 디자인관리", "email@mail.com","김상덕",76,"프론트엔드","자바",3);
-		recruimentDto test2 = new recruimentDto("이메일 관리", "1email@mail.com", "길드", 79,"백엔드","c",1);
-		recruimentDto test3 = new recruimentDto("드론 프로젝트", "3email@mail.com", "라이트", 77,"백엔드","python",1);
+		recruimentDto test1 = new recruimentDto("캡스톤 디자인관리", "email@mail.com","김상덕",76);
+		recruimentDto test2 = new recruimentDto("이메일 관리", "1email@mail.com", "길드", 79);
+		recruimentDto test3 = new recruimentDto("드론 프로젝트", "3email@mail.com", "라이트", 77);
 		
 		recruimentList.add(test1);
 		recruimentList.add(test2);
 		recruimentList.add(test3);
 		
-		
-		
+		List<recruimentmemberDto> recruimentmemberList = new ArrayList<>();//프로젝트 리스트
+		recruimentmemberDto test11 = new recruimentmemberDto("프론트엔드","자바",3);
+		recruimentmemberDto test12 = new recruimentmemberDto("백엔드","C",1);
+		recruimentmemberDto test13 = new recruimentmemberDto("백엔드","C#",2);
+
+		recruimentmemberList.add(test11);
+		recruimentmemberList.add(test12);
+		recruimentmemberList.add(test13);
+	
+		model.addAttribute("recruiment_member",recruimentmemberList);
 		model.addAttribute("recruiment_",recruimentList);
 		return "/recruimentboard";
 	}
