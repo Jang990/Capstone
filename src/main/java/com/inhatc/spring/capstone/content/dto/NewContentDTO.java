f012209ab324d6c0118f8ec3542ab50ca663d4cc
Jang_Desktop <sdsd090811@gmail.com>
2022-12-11 16:27:58 +0900

    Feat: tag 관련 dto 추가

--- src/main/java/com/inhatc/spring/capstone/content/dto/NewContentDTO.java
+++ src/main/java/com/inhatc/spring/capstone/content/dto/NewContentDTO.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.inhatc.spring.capstone.file.dto.DisplayedImageDTO;
+import com.inhatc.spring.capstone.tag.dto.DisplayedTagDTO;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 /** 콘텐츠 수정 또는 생성 시 사용되는 DTO */
 public class NewContentDTO {
@@ -25,6 +28,7 @@ public class NewContentDTO {
 	private boolean isRecruit;
 	
 	List<DisplayedImageDTO> images = new ArrayList<>();
+	List<DisplayedTagDTO> tags = new ArrayList<>();
 
 	@Builder
 	public NewContentDTO(Long contentId, Long userId, String userEmail, String title, String content, 
