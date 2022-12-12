600eb5af7168bbb563366722c8f02c4fa5a38951
Jang_Desktop <sdsd090811@gmail.com>
2022-12-12 11:45:43 +0900

    Test: save 테스트 코드 추가

--- src/test/java/com/inhatc/spring/capstone/tag/service/TagServiceTest.java
+++ src/test/java/com/inhatc/spring/capstone/tag/service/TagServiceTest.java
@@ -2,6 +2,7 @@ package com.inhatc.spring.capstone.tag.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.inhatc.spring.capstone.tag.constant.TagType;
 import com.inhatc.spring.capstone.tag.dto.DisplayedTagDTO;
 import com.inhatc.spring.capstone.tag.entity.Tag;
 import com.inhatc.spring.capstone.tag.repository.TagRepository;
@@ -21,6 +23,9 @@ class TagServiceTest {
 
 	@Autowired
 	TagRepository tagRepository;
+	
+	@Autowired
+	TagService tagService;
 	
 	@Test
 	@DisplayName("단순 QueryDSL 확인용 테스트")
@@ -37,5 +42,38 @@ class TagServiceTest {
 		
 		List<DisplayedTagDTO> simlarTags = tagRepository.getSimilarTags("테스트관련Ta", PageRequest.of(0, 10));
 	}
-
+	
+	@Test
+	@DisplayName("커스텀 태그저장 기능 테스트")
+	@Transactional
+	void saveCustomTagTest() {
+		List<DisplayedTagDTO> newTagList = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			newTagList.add(createNewTag("Test 생성 태그" + i));
+		}
+		
+		List<DisplayedTagDTO> savedTagList = new ArrayList<>();
+		for (DisplayedTagDTO displayedTagDTO : newTagList) {
+			savedTagList.add(tagService.saveCustomTag(displayedTagDTO));
+		}
+		
+		for (int i = 0; i < savedTagList.size(); i++) {
+			DisplayedTagDTO newTag = newTagList.get(i);
+			DisplayedTagDTO savedTag = savedTagList.get(i);
+			
+			assertEquals(newTag.getTagName(), savedTag.getTagName());
+			assertEquals(newTag.getTagType(), TagType.NEW.toString());
+			assertEquals(savedTag.getTagType(), TagType.CUSTOM.toString());
+			assertNotEquals(savedTag.getTagId(), null);
+		}
+	}
+	
+	// 프론트엔드에서 새로 만들어진 태그
+	DisplayedTagDTO createNewTag(String tagName) {
+		return DisplayedTagDTO.builder()
+				.tagName(tagName)
+				.tagType(TagType.NEW.toString())
+				.build();
+	}
+	
 }
