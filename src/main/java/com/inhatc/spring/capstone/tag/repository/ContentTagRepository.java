30b850d24d6e8a810b73d41a81133e789a727f2c
Jang_Desktop <sdsd090811@gmail.com>
2022-12-12 17:40:47 +0900

    Fix: ContentTag 레포지토리도 복구

--- src/main/java/com/inhatc/spring/capstone/tag/repository/ContentTagRepository.java
+++ src/main/java/com/inhatc/spring/capstone/tag/repository/ContentTagRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.inhatc.spring.capstone.tag.entity.ContentTag;
 
-public interface ContentTagRepository 
-	// extends JpaRepository<ContentTag, Long> 
+public interface ContentTagRepository extends JpaRepository<ContentTag, Long> 
 {
 	
 }
