c894839beeb9b9b0622d6effdc649720aeeb6c18
Jang_Desktop <sdsd090811@gmail.com>
2022-12-12 12:02:22 +0900

    Test: 이미 저장되어 있는 태그 포함 save 테스트

--- src/test/java/com/inhatc/spring/capstone/tag/service/TagServiceTest.java
+++ src/test/java/com/inhatc/spring/capstone/tag/service/TagServiceTest.java
@@ -68,6 +68,34 @@ class TagServiceTest {
 		}
 	}
 	
+	@Test
+	@DisplayName("이미 저장되어 있는 태그 포함 save 테스트")
+	@Transactional
+	void saveCustomTagTest2() {
+		List<DisplayedTagDTO> newTagList = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			if(i%2 == 0)
+				newTagList.add(createNewTag("Test 생성 태그" + i));
+			else
+				newTagList.add(createSavedCustomTag("저장되어 있는 태그"+i));
+		}
+		
+		List<DisplayedTagDTO> savedTagList = new ArrayList<>();
+		for (DisplayedTagDTO displayedTagDTO : newTagList) {
+			savedTagList.add(tagService.saveCustomTag(displayedTagDTO));
+		}
+		
+		for (int i = 0; i < savedTagList.size(); i++) {
+			DisplayedTagDTO newTag = newTagList.get(i);
+			DisplayedTagDTO savedTag = savedTagList.get(i);
+			System.out.println(savedTag);
+			
+			assertEquals(newTag.getTagName(), savedTag.getTagName());
+			assertEquals(savedTag.getTagType(), TagType.CUSTOM.toString());
+			assertNotEquals(savedTag.getTagId(), null);
+		}
+	}
+	
 	// 프론트엔드에서 새로 만들어진 태그
 	DisplayedTagDTO createNewTag(String tagName) {
 		return DisplayedTagDTO.builder()
@@ -75,5 +103,49 @@ class TagServiceTest {
 				.tagType(TagType.NEW.toString())
 				.build();
 	}
+	
+	// 저장되어 있는 Custom 태그
+	DisplayedTagDTO createSavedCustomTag(String tagName) {
+		DisplayedTagDTO displayedTagDTO = DisplayedTagDTO.builder()
+			.tagName(tagName)
+			.tagType(TagType.NEW.toString())
+			.build();
+		
+		return tagService.saveCustomTag(displayedTagDTO);
+	}
+	
+	// 에러 발생 태그(id가 없지만 New가 아님)
+	DisplayedTagDTO createErrorTag(String tagName) {
+		return DisplayedTagDTO.builder()
+				.tagName(tagName)
+				.tagType(TagType.TECH.toString())
+				.build();
+	}
+	
+	// 에러 발생 태그(id 존재 하지 않음)
+	DisplayedTagDTO createNotFoundErrorTag(String tagName) {
+		return DisplayedTagDTO.builder()
+				.tagId(Long.MIN_VALUE)
+				.tagName(tagName)
+				.tagType(TagType.CUSTOM.toString())
+				.build(); 
+	}
+	
+	// 에러 발생 태그(기존 저장된 id와 tagName이 매칭되지 않음)
+	DisplayedTagDTO craeteMismatchErrorTag(String tagName) {
+		DisplayedTagDTO displayedTagDTO = DisplayedTagDTO.builder()
+				.tagName(tagName)
+				.tagType(TagType.NEW.toString())
+				.build();
+			
+		displayedTagDTO = tagService.saveCustomTag(displayedTagDTO);
+		
+		return DisplayedTagDTO.builder()
+				.tagId(Long.MIN_VALUE)
+				.tagName(displayedTagDTO.getTagName())
+				.tagType(TagType.TECH.toString())
+				.build(); 
+	}		
+	
 	
 }
