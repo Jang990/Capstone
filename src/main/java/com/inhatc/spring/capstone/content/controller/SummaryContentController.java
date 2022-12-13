b3396b2e33192d7c03d25a6f81bbfd3621bf89a8
Jang_Desktop <sdsd090811@gmail.com>
2022-12-13 17:23:37 +0900

    Feat: 컨트롤러에서 검색어, sort를 받을 수 있도록 수정

--- src/main/java/com/inhatc/spring/capstone/content/controller/SummaryContentController.java
+++ src/main/java/com/inhatc/spring/capstone/content/controller/SummaryContentController.java
@@ -1,15 +1,18 @@
 package com.inhatc.spring.capstone.content.controller;
 
-import java.util.List;
+import java.util.List; 
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inhatc.spring.capstone.content.dto.DisplayedSummaryContentDTO;
-import com.inhatc.spring.capstone.content.service.ContentSummaryService;
+import com.inhatc.spring.capstone.content.service.ContentService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,11 +21,18 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/summary-content")
 public class SummaryContentController {
 	
-	private final ContentSummaryService contentSummaryService;
+	private final ContentService contentService;
 	
 	@GetMapping
-	public List<DisplayedSummaryContentDTO> getContentList(@PageableDefault(page = 0, size = 6) Pageable pageable) {
-		
+	public List<DisplayedSummaryContentDTO> getContentList(@PageableDefault(page = 0, size = 6) 
+		@SortDefault.SortDefaults({ @SortDefault(sort = "date_created", direction = Sort.Direction.DESC)}) Pageable pageable,
+		 @RequestParam(value = "search", required = false) String search) {
+		if(search == null) {
+			contentService.getSummaryContents(pageable);
+		}
+		else {
+			// 일단 검색기능은 컨트롤러 만들고 설정하기
+		}
 		return null;
 	}
 }
