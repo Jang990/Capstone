153eb11efb396e01b081d0d2d557a632b6f9e70b
Jang_Desktop <sdsd090811@gmail.com>
2022-12-13 22:14:40 +0900

    Feat: 상위태그 빼내도록 변경

--- src/main/java/com/inhatc/spring/capstone/tag/repository/TagRepositoryImpl.java
+++ src/main/java/com/inhatc/spring/capstone/tag/repository/TagRepositoryImpl.java
@@ -6,9 +6,11 @@ import static com.querydsl.core.group.GroupBy.groupBy;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.thymeleaf.util.StringUtils;
 
 import com.inhatc.spring.capstone.tag.dto.DisplayedTagDTO;
 import com.inhatc.spring.capstone.tag.dto.QDisplayedTagDTO;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -18,8 +20,13 @@ public class TagRepositoryImpl implements TagRepositoryCustom {
 	private final JPAQueryFactory query;
 	
 	public List<DisplayedTagDTO> getSimilarTags(String tagKeyword, Pageable pageable) {
+		BooleanBuilder builder = new BooleanBuilder();
+		if(!StringUtils.isEmpty(tagKeyword)) {
+			builder.or(tag.name.contains(tagKeyword));
+		}
+		
 		return  query.selectFrom(tag)
-				.where(tag.name.contains(tagKeyword))
+				.where(builder)
 				.offset(pageable.getOffset())
 				.limit(pageable.getPageSize())
 				.orderBy(tag.taggedCount.desc())
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO capstonemanagement.tag
+(name, tagged_count, `type`)
+VALUES
+('java', 1, 'TECH'),
+('c', 2, 'TECH'),
+('csharp', 3, 'TECH'),
+('codepen', 4, 'TECH'),
+('git', 5, 'TECH');
