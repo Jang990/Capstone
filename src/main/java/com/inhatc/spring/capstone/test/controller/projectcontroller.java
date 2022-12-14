f50a8ac730b2669a2d40702d75286ce63ebc75e8
Jang_Desktop <sdsd090811@gmail.com>
2022-12-14 17:24:04 +0900

    Feat: 프로젝트 보드 데이터 연결 추가

--- src/main/java/com/inhatc/spring/capstone/test/controller/projectcontroller.java
+++ src/main/java/com/inhatc/spring/capstone/test/controller/projectcontroller.java
@@ -4,21 +4,32 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.inhatc.spring.capstone.content.dto.DisplayedContentDTO;
+import com.inhatc.spring.capstone.content.dto.DisplayedSummaryContentDTO;
+import com.inhatc.spring.capstone.content.service.ContentService;
 
 import groovy.util.logging.Log4j2;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Controller
+@RequiredArgsConstructor
 public class projectcontroller {
 	
+	private final ContentService contentService;
 	
 	@Getter
 	@Setter
@@ -41,9 +52,17 @@ public class projectcontroller {
 	}
 	
 	@GetMapping("/projectboard")
-	public String projectboard(Model model) {
+	public String projectboard(Model model,
+			@PageableDefault(page = 0, size = 6) 
+			@SortDefault.SortDefaults({ @SortDefault(sort = "heart", direction = Sort.Direction.DESC)}) 
+			Pageable pageable,
+			@RequestParam(value = "keywords", required = false, defaultValue = "") String keywords,
+			@RequestParam(value = "email", required = false, defaultValue = "") String email
+	) {
+		Page<DisplayedSummaryContentDTO> summaryContents = contentService.getSummaryContents(pageable, keywords, email);
+		model.addAttribute("summaryDto", summaryContents); // 정보
+		model.addAttribute("keywords", keywords); // 검색어 
 		
-
 		List<projectDto> projectList = new ArrayList<>();//프로젝트 리스트
 		projectDto test1 = new projectDto("캡스톤 디자인관리", "email@mail.com", "김상덕", "2012-12-12", "자바", 76);
 		projectDto test2 = new projectDto("이메일 관리", "1email@mail.com", "길드", "2012-12-17", "python", 79);
@@ -52,6 +71,7 @@ public class projectcontroller {
 		projectList.add(test1);
 		projectList.add(test2);
 		projectList.add(test3);
+		
 		
 		model.addAttribute("project_",projectList);
 		return "/projectboard";
