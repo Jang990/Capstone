cc9055a3cef20ca21a8992e31dfa7aea02db37f5
Jang_Desktop <sdsd090811@gmail.com>
2022-12-14 17:30:39 +0900

    Feat: myprojectboard 데이터 연결 추가

--- src/main/java/com/inhatc/spring/capstone/test/controller/myPagecontroller.java
+++ src/main/java/com/inhatc/spring/capstone/test/controller/myPagecontroller.java
@@ -3,22 +3,35 @@ package com.inhatc.spring.capstone.test.controller;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.inhatc.spring.capstone.content.dto.DisplayedContentDTO;
+import com.inhatc.spring.capstone.content.dto.DisplayedSummaryContentDTO;
+import com.inhatc.spring.capstone.content.service.ContentService;
 
 import groovy.util.logging.Log4j2;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Controller
+@RequiredArgsConstructor
 public class myPagecontroller {
-	
+
+	private final ContentService contentService;
 	
 	@Getter
 	@Setter
@@ -41,8 +54,17 @@ public class myPagecontroller {
 	}
 	
 	@GetMapping("/myprojectboard")
-	public String myprojectboard(Model model) {
+	public String myprojectboard(Model model, 
+			@PageableDefault(page = 0, size = 6) 
+			@SortDefault.SortDefaults({ @SortDefault(sort = "heart", direction = Sort.Direction.DESC)}) 
+			Pageable pageable,
+			Authentication authentication
+	) {
+		OAuth2User user = (OAuth2User) authentication.getPrincipal();
+		String loginEmail = (String) user.getAttributes().get("email");
 		
+		Page<DisplayedSummaryContentDTO> summaryContents = contentService.getSummaryContents(pageable, null, loginEmail);
+		model.addAttribute("summaryDto", summaryContents); // 정보
 
 		List<myprojectDto> myprojectList = new ArrayList<>();//프로젝트 리스트
 		myprojectDto test1 = new myprojectDto("캡스톤 디자인관리", "email@mail.com", "김상덕", "2012-12-12", "자바", 76);
