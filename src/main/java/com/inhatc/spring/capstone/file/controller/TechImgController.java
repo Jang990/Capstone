99cfa7c4b2dc6124c7d3b03ce32529ad061a3910
Jang_Desktop <sdsd090811@gmail.com>
2022-12-14 21:14:02 +0900

    Feat: Tech 태그에 해당하는 이미지 가져오기 추가

--- src/main/java/com/inhatc/spring/capstone/file/controller/TechImgController.java
+++ src/main/java/com/inhatc/spring/capstone/file/controller/TechImgController.java
@@ -1,8 +1,11 @@
 package com.inhatc.spring.capstone.file.controller;
 
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +22,19 @@ public class TechImgController {
 	
 	/** Tech 태그에 해당하는 리소스 위치 가져오기 */
 	@GetMapping("/resource-loc/{tagId}")
-	public ResponseEntity<String> getTechTagResourceLocation(Long tagId) {
-		techImgService.getTechImgResourceSrc(tagId);
+	public ResponseEntity<String> getTechTagResourceLocation(@PathVariable Long tagId) {
+		String path = null;
+		try {
+			path = techImgService.getTechImgResourceSrc(tagId);
+		}
+		catch (IOException e) {
+			// 파일 불러오기 실패 - 로그 남겨야 함
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 		
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(path ,HttpStatus.OK);
 	}
 }
--- src/main/java/com/inhatc/spring/capstone/file/service/TechImgService.java
+++ src/main/java/com/inhatc/spring/capstone/file/service/TechImgService.java
@@ -1,10 +1,14 @@
 package com.inhatc.spring.capstone.file.service;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
 import com.inhatc.spring.capstone.tag.constant.TagType;
 import com.inhatc.spring.capstone.tag.entity.Tag;
@@ -16,16 +20,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TechImgService {
 	private final TagRepository tagRepository;
+	
+	@Value(value = "${techIconLocation}")
+	private String techLocation;
 
-	public String getTechImgResourceSrc(Long tagId) {
+	public String getTechImgResourceSrc(Long tagId) throws IOException {
 		Tag tag = tagRepository.findById(tagId)
 				.orElseThrow(EntityNotFoundException::new);
 		
 		if(tag.getType() != TagType.TECH) 
 			throw new IllegalArgumentException();
 		
+		ClassPathResource cpr = new ClassPathResource(techLocation+"/"+tag.getName().toLowerCase());
+		File file = cpr.getFile();
+		File[] listFiles = file.listFiles();
 		
+		for (File file2 : listFiles) {
+			String fileName = file2.getName();
+			String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
+			if(ext.equals("svg")) {
+				return "./devicon/icons/"+tag.getName()+"/"+fileName;
+			}
+		}
 		
-		return null;
+		throw new IllegalArgumentException();
 	}
 }
