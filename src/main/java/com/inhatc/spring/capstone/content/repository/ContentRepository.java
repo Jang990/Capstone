94ed4a6dc86f3859956df37fe6e404efd9ddcc92
simbonggyo <simbonggyo@gmail.com>
2022-12-14 16:59:43 +0900

    수정

--- src/main/java/com/inhatc/spring/capstone/content/repository/ContentRepository.java
+++ src/main/java/com/inhatc/spring/capstone/content/repository/ContentRepository.java
@@ -1,7 +1,6 @@
 package com.inhatc.spring.capstone.content.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.inhatc.spring.capstone.content.entity.Content;
 
 public interface ContentRepository extends JpaRepository<Content, Long>, ContentRepositoryCustom {
