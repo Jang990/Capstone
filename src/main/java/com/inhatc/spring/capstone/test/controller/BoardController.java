e4874db88961fc961418ec85eb888faf8fa7d56d
Jang_Desktop <sdsd090811@gmail.com>
2022-12-15 09:07:33 +0900

    Refactor: 필요없는 파일 삭제

--- src/main/java/com/inhatc/spring/capstone/test/controller/BoardController.java
+++ src/main/java/com/inhatc/spring/capstone/test/controller/BoardController.java
@@ -1,44 +1,23 @@
 package com.inhatc.spring.capstone.test.controller;
 
 
-import java.io.IOException;
+import java.io.IOException; 
 
-
-
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.kohsuke.github.GHEventPayload.Public;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.google.gson.JsonObject;
-import com.inhatc.spring.capstone.entity.board.board;
 import com.inhatc.spring.capstone.tag.constant.TagType;
 import com.inhatc.spring.capstone.tag.dto.DisplayedTagDTO;
-import com.inhatc.spring.capstone.test.service.boardservice;
-import com.querydsl.core.util.FileUtils;
 
-import groovy.util.logging.Log4j2;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import com.inhatc.spring.capstone.content.service.ContentService;
 import com.inhatc.spring.capstone.content.dto.NewContentDTO;
-import com.inhatc.spring.capstone.content.entity.Content;
 
 @Controller
 
@@ -46,9 +25,6 @@ public class BoardController {
 	
 	@Autowired
 	private ContentService contentService;
-	
-	@Autowired
-	private boardservice boardservice;
 	
     @GetMapping("editor/editor3")
     public String editorboard() {
@@ -76,13 +52,11 @@ public class BoardController {
         content.setTags(tagList);
         
         
-//        /*
         try {
 			contentService.createProjectContent(content);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-//        */
 		return "/main";
 	
 	}
