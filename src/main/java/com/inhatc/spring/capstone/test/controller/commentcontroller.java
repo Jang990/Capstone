d1e9ae04b46433f8ce37d4f4c15730bfd3a193a1
simbonggyo <simbonggyo@gmail.com>
2022-12-15 11:45:54 +0900

    최신화

--- src/main/java/com/inhatc/spring/capstone/test/controller/commentcontroller.java
+++ src/main/java/com/inhatc/spring/capstone/test/controller/commentcontroller.java
@@ -1,10 +1,9 @@
 package com.inhatc.spring.capstone.test.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import com.inhatc.spring.capstone.content.dto.DisplayedCommentDTO;
 
